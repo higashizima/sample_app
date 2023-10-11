a634c22634aac39279550b2b0efc7806a65e7187
Mika Higashizima <144198758+higashizima@users.noreply.github.com>
2023-10-11 04:25:50 +0000

    Initial Commit2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,9 +5,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to 'top'
+     @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+       render :new
+    end
   end
 
   def index
@@ -19,11 +22,24 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
 
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
